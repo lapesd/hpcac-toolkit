4fec3cb444bf9c4df67a7eec88f2f1436f5ee039
Prog-in <joaogabrielferes@gmail.com>
2023-11-23 11:00:21 -0300

    create install_scr.sh and minor fixes

---
+++ scripts/mpi/install_scr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCR_ROOT="$HOME/scr"
+
+echo "export SCR_ROOT=\"$SCR_ROOT\"" >> ~/.bashrc
+source ~/.bashrc
+
+# Build and install SCR (Scalable Checkpoint / Restart)
+wget https://github.com/LLNL/scr/releases/download/v3.0/scr-v3.0.tgz
+tar -zxf scr-v3.0.tgz
+cd scr-v3.0
+
+mkdir build
+cd build
+cmake -DSCR_RESOURCE_MANAGER=NONE -DCMAKE_INSTALL_PREFIX=$SCR_ROOT ..
+make -j install
