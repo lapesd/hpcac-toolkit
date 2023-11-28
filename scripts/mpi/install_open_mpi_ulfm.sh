1173cac080af20d32a3094b400223cbdd0f466cd
Prog-in <joaogabrielferes@gmail.com>
2023-11-28 15:03:15 -0300

    add info about open mpi instalation

--- scripts/mpi/install_open_mpi_ulfm.sh
+++ scripts/mpi/install_open_mpi_ulfm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Need package "flex" to be installed without errors
+
 # Build and install OpenMPI with ULFM (User-Level Failure Mitigation) support from source
 git clone --recursive https://github.com/open-mpi/ompi.git
 cd ompi || exit
