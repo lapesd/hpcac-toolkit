a5f425d0a51a3279f70af4d3578ae2df54bfc9b5
ArturRSoda <artursodar@gmail.com>
2025-07-22 18:49:09 -0300

    saving run-tasks outputs

--- src/commands/cluster/run_task.rs
+++ src/commands/cluster/run_task.rs
@@ -1,16 +1,18 @@
 use crate::database::models::{ Cluster, ProviderConfig };
-use crate::integrations::{cloud_interface::CloudResourceManager, providers::aws::AwsInterface};
-use crate::database::models::ClusterState;
+use crate::integrations::providers::aws::AwsInterface;
+use crate::database::models::{ ClusterState, InstanceType };
 use crate::utils;
 
-use std::{thread, time};
-
+use std::time;
 use serde::{Deserialize, Serialize};
 use anyhow::{Result, bail};
 use sqlx::sqlite::SqlitePool;
 use std::fs;
 use std::path::Path;
 use tracing::{info, error};
+use aws_sdk_ec2::types::Filter;
+use chrono::Local;
+use std::io::Write;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct TasksYaml {
@@ -19,8 +21,9 @@ struct TasksYaml {
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Task {
-    task_name: String,
-    commands: Vec<String>,
+    task_tag: String,
+    setup_commands: Vec<String>,
+    run_commands: Vec<String>,
 }
 
 pub async fn run_task(
@@ -65,13 +68,12 @@ pub async fn run_task(
             return Ok(());
         }
     };
+    let nodes = cluster.get_nodes(pool).await?;
 
     if cluster.state != ClusterState::Running {
-        println!("Cluster '{}' was not spawned.", cluster_id);
+        println!("Cluster with id '{}' was not spawned.", cluster_id);
         return Ok(());
     }
-
-    let nodes = cluster.get_nodes(pool).await?;
 
     // Get cloud interface
     let provider_config =
@@ -95,12 +97,16 @@ pub async fn run_task(
     // Confirm with user
     println!("Tasks:");
     for task in tasks_yaml.tasks.iter() {
-        println!(" - name: {}", task.task_name);
-        println!(" - commands:");
-        for command in task.commands.iter() {
+        println!(" - name: {}", task.task_tag);
+        println!("   setup_commands:");
+        for command in task.setup_commands.iter() {
             println!("     - {}", command);
         }
-        println!("");
+        println!("   run_commands:");
+        for command in task.run_commands.iter() {
+            println!("     - {}", command);
+        }
+        println!();
     }
     if !(utils::user_confirmation(
         skip_confirmation,
@@ -108,43 +114,150 @@ pub async fn run_task(
     )?) {
         return Ok(());
     }
-    println!("");
+    println!();
 
-    // Get context and master node ec2 id
+    // Get context and task_runner_instance_id
     let context = cloud_interface.create_cluster_context(&cluster)?;
-    //let ec2_instance_id = &context.ec2_instance_ids.get(&0).unwrap();
-    let ec2_instance_id = "i-0f3fe6cf06e65b9d3";
+    let task_runner_instance_name = context.ec2_instance_name(0);
+    let filter = Filter::builder()
+        .name("tag:Name")
+        .values(&task_runner_instance_name)
+        .build();
+    let resp = context.ec2_client.describe_instances()
+        .filters(filter)
+        .send()
+        .await?;
+    let ec2_id = resp.reservations()
+        .iter()
+        .flat_map(|r| r.instances())
+        .find_map(|i| i.instance_id().map(|id| id.to_string()));
+    let task_runner_instance_ec2_id = match ec2_id {
+        Some(id) => id,
+        None     => bail!("Unable to retrieve ec2 instance id."),
+    };
+
+    // Report result string
+    let mut report_str = String::new();
+    report_str.push_str(&format!("-=-=-=-=-=-=-=-= CLUSTER DETAILS =-=-=-=-=-=-=-=-\n"));
+    report_str.push_str(&format!("{:<35}: {}\n", "Cluster Name", cluster.display_name));
+    report_str.push_str(&format!("{:<35}: {}\n", "Provider", cluster.provider_id));
+    report_str.push_str(&format!("{:<35}: {}\n", "Region", cluster.region));
+    report_str.push_str(&format!("{:<35}: {}\n", "Availability Zone", cluster.availability_zone));
+    report_str.push_str(&format!("{:<35}: {}\n", "Use Node Affinity", cluster.use_node_affinity));
+    report_str.push_str(&format!("{:<35}: {}\n", "Use Elastic Fabric Adapters (EFAs)", cluster.use_elastic_fabric_adapters));
+    report_str.push_str(&format!("{:<35}: {}\n", "Use Elastic File System (EFS)", cluster.use_elastic_file_system));
+    report_str.push_str(&format!("{:<35}: {}\n", "On Instance Creation Failure", cluster.on_instance_creation_failure.clone().unwrap().to_string()));
+    report_str.push_str(&format!("{:<35}: {}\n", "Provider Config", provider_config.display_name));
+    report_str.push_str(&format!("{:<35}: {}\n\n", "Node Count", nodes.len()));
+
+    report_str.push_str(&format!("Node Details:\n"));
+    for (i, node) in nodes.iter().enumerate() {
+        let instance_type_name = &node.instance_type;
+        let instance_details = InstanceType::fetch_by_name_and_region(pool, instance_type_name, &cluster.region).await?.unwrap();
+        let processor_info = match &instance_details.core_count {
+            Some(cores) => {
+                format!(
+                    "{}-Core {} {}",
+                    cores, instance_details.cpu_architecture, instance_details.cpu_type
+                )
+            }
+            None => {
+                format!(
+                    "{} {}",
+                    instance_details.cpu_architecture, instance_details.cpu_type
+                )
+            }
+        };
+
+        let gpu_info = match instance_details.gpu_type {
+            Some(gpu) => {
+                format!("{}x {}", instance_details.gpu_count, gpu)
+            }
+            None => "N/A".to_string(),
+        };
+
+        report_str.push_str(&format!("  Node {}:\n", i + 1));
+        report_str.push_str(&format!("    Instance Type   : {}\n", node.instance_type));
+        report_str.push_str(&format!("    Processor       : {}\n", processor_info));
+        report_str.push_str(&format!("    vCPUs:          : {}\n", instance_details.vcpus));
+        report_str.push_str(&format!("    GPUs:           : {}\n", gpu_info));
+        report_str.push_str(&format!("    Image ID        : {}\n", node.image_id));
+        report_str.push_str(&format!("    Allocation Mode : {}\n", node.allocation_mode));
+        report_str.push_str(&format!("    Burstable Mode  : {}\n",
+            node.burstable_mode.as_deref().unwrap_or("N/A")
+        ));
+    }
+    report_str.push_str(&format!("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"));
 
     // Running tasks
-    let steps: usize = tasks_yaml.tasks.iter().map(|task| task.commands.len()).sum();
-    let mut output = String::new();
+    let steps: usize = tasks_yaml.tasks.iter()
+        .fold(0, |acc, task| acc + task.setup_commands.len() + task.run_commands.len());
+
     let multi = utils::ProgressTracker::create_multi();
     let main_progress = utils::ProgressTracker::add_to_multi(&multi, steps as u64, Some("Initializing..."));
     let operation_spinner = utils::ProgressTracker::new_indeterminate(&multi, "Initializing...");
-    for task in tasks_yaml.tasks.iter() {
-        output.push_str(&format!(" - task {}\n", task.task_name));
 
-        let running_task_message = format!("Running task '{}'...", task.task_name);
+    for task in tasks_yaml.tasks.iter() {
+        report_str.push_str(&format!("===> Task: '{}'\n", task.task_tag));
+
+        let running_task_message = format!("Running task '{}' setup commands...", task.task_tag);
         info!(running_task_message);
         main_progress.update_message(&running_task_message);
 
-        for command in task.commands.iter() {
+        for command in task.setup_commands.iter() {
             operation_spinner.update_message(&format!("Executing command: '{}'", command));
-            output.push_str(&format!("$ {}\n", command));
+            report_str.push_str(&format!("$ {}\n", command));
             
-            match cloud_interface.send_and_wait_for_ssm_command(&context, ec2_instance_id, command.clone()).await {
-                Ok(out) => output.push_str(&format!("{}\n", out)),
-                Err(e)     => output.push_str(&format!("error: {}\n", e)),
+            match cloud_interface.send_and_wait_for_ssm_command(&context, &task_runner_instance_ec2_id, command.clone()).await {
+                Ok(out) => report_str.push_str(&format!("{}\n", out)),
+                Err(e)  => report_str.push_str(&format!("error: {}\n\n", e)),
             }
 
-            //thread::sleep(time::Duration::from_secs(5));
             main_progress.inc(1);
         }
+
+        let running_task_message = format!("Running task '{}' run_commands...", task.task_tag);
+        info!(running_task_message);
+        main_progress.update_message(&running_task_message);
+
+        let run_commands_start = time::Instant::now();
+        for command in task.run_commands.iter() {
+            operation_spinner.update_message(&format!("Executing command: '{}'", command));
+            report_str.push_str(&format!("$ {}\n", command));
+            
+            match cloud_interface.send_and_wait_for_ssm_command(&context, &task_runner_instance_ec2_id, command.clone()).await {
+                Ok(out) => report_str.push_str(&format!("{}\n", out)),
+                Err(e)  => report_str.push_str(&format!("{}\n\n", e)),
+            }
+
+            main_progress.inc(1);
+        }
+
+        let elapsed_ms = run_commands_start.elapsed().as_millis();
+        let elapsed_secs = elapsed_ms as f64 / 1000.0;
+        report_str.push_str(&format!("===== End of Task '{}' - exec time: {:.3} s =====\n\n", task.task_tag, elapsed_secs));
     }
 
-    operation_spinner.finish_with_message("All commands of all tasks executed successfully!");
-    main_progress.finish_with_message("All tasks executed successfully!");
+    operation_spinner.finish_with_message("All commands of all tasks completed!");
+    main_progress.finish_with_message("All tasks completed!");
+    info!("All tasks completed!");
 
-    println!("{}", output);
+    // save report
+    let mut created_dir = true;
+    let report_path = format!("results/cluster_{}", cluster_id);
+    match fs::create_dir_all(&report_path) {
+        Ok(v) => v,
+        Err(e) => { println!("Unable to create directory for result report: {}", e); created_dir = false; },
+    };
+
+    if created_dir {
+        let timestamp = Local::now().format("%Y-%m-%dT%H:%M:%S");
+        let filename = format!("{}.txt", timestamp);
+        let mut file = fs::File::create(&format!("{}/{}", report_path, filename))?;
+        writeln!(file, "{}", report_str)?;
+        println!("Result report saved at '{}/{}'", report_path, filename);
+    }
+
     Ok(())
 }
+
